fc5b8c14f6985ff8f6fb28e8af76185704843fd0
Evgeniya <prahova.e@ya.ru>
2023-03-07 15:58:07 +0500

    3 tasks

---
+++ task56/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[] FindRowSum(int[,] matrix)
+{
+    int[] sum = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int rowSum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            rowSum += matrix[i, j];
+        }
+        sum[i] = rowSum;
+    }
+    return sum;
+}
+
+void FindMinSum(int[] array)
+{
+    int minSum = array[0];
+    int count = 1;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (minSum > array[i])
+        {
+            minSum = array[i];
+            count = i+1;
+        }
+    }
+    System.Console.WriteLine($"Номер строки с наименьшей суммой элементов: {count} строка. Сумма элементов: {minSum}");
+}       
+
+int rows = ReadInt("Введите колчество строк матрицы: ");
+int cols = ReadInt("Введите колчество столбцов матрицы:");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+FindMinSum(FindRowSum(myMatrix));
---
+++ task60/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,,] GenerateMatrix(int quantity1, int quantity2, int quantity3)
+{
+    Random rand = new Random();
+    int[,,] matrix = new int[quantity1, quantity2, quantity3];
+    for (int i = 0; i < quantity1; i++)
+    {
+        for (int j = 0; j < quantity2; j++)
+        {
+            for (int k = 0; k < quantity3; k++)
+            {
+                matrix[i, j, k] = rand.Next(10, 100);
+            }
+
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                System.Console.Write(matrix[i, j, k] + "(" + i + ", " + j + ", " + k + ")" + "\t");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int quantity1 = ReadInt("Введите колчество элеметов для первого измерения: ");
+int quantity2 = ReadInt("Введите колчество элеметов для второго измерения: ");
+int quantity3 = ReadInt("Введите колчество элеметов для третьего измерения: ");
+var myMatrix = GenerateMatrix(quantity1, quantity2, quantity3);
+PrintMatrix(myMatrix);
